88bdbab0c54c8e6f9b67298ec300d23b7a54e94f
Reid Weber <reid@thesufferfest.com>
2021-12-09 14:44:59 -0800

    revert manufacturer ID back to SUF (#16)

--- src/fitparser.cc
+++ src/fitparser.cc
@@ -317,7 +317,7 @@ void FitParser::Encode(const v8::FunctionCallbackInfo<v8::Value> &args)
   // FileIdMesg SECTION
   fit::FileIdMesg fileIdMesg; // Every FIT file requires a File ID message
   fileIdMesg.SetType(FIT_FILE_ACTIVITY);
-  fileIdMesg.SetManufacturer(FIT_MANUFACTURER_WAHOO_FITNESS);
+  fileIdMesg.SetManufacturer(FIT_MANUFACTURER_THE_SUFFERFEST);
   fileIdMesg.SetProduct(1231);
   fileIdMesg.SetSerialNumber(12345);
   fileIdMesg.SetTimeCreated(GET_INT("timeCreated"));
